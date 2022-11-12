7107d57f21e8436f22d8d0777126ddd6a4825b20
rreichert01 <rodneyreichert2024@u.northwestern.edu>
2022-11-12 12:47:58 -0600

    testing get_ipv4

--- queries.py
+++ queries.py
@@ -11,7 +11,7 @@ def process_txt(file):
 
 def get_ipv4(website):
     dns_servers = process_txt("dns-servers.txt")
-    for dns_server in dns_servers[:1]:
+    for dns_server in dns_servers[:3]:
         result = subprocess.check_output(["nslookup", website, dns_server],
                                          timeout=2, stderr=subprocess.STDOUT).decode("utf - 8")
         print(result)
